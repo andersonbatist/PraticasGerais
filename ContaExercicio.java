afe0290b31a5007adfdc29f51b22ed02769ac3ee
andersonbatist <andersonbatista.home@gmail.com>
2026-09-22 23:53:39 -0300

    Novas adições/ajustes de métodos

--- ContaExercicio.java
+++ ContaExercicio.java
@@ -2,17 +2,38 @@ public class ContaExercicio {
 
     //Atributos da minha classe:
     private String nomeTitular;
-    private String dataAbertura;
     private String agencia;
     private int numero;
     private double saldo;
+    private Data data;
+
+    public void ex (int d, int m, int a) {
+
+        this.data = new Data();
+
+        this.data.dia = d;
+        this.data.mes = m;
+        this.data.ano = a;
+
+        System.out.printf(
+            "Data: %02d/%02d/%04d%n",
+            data.dia,
+            data.mes,
+            data.ano
+        ); 
+    }
 
 
 
     //Construtor para as contas
-    ContaExercicio (String nomeT, String dataA, String agenciA, int numerO, double saldO) {
+    ContaExercicio (String nomeT, int d, int m, int a, String agenciA, int numerO, double saldO) {
+
+        this.data = new Data();
+
         this.nomeTitular = nomeT;
-        this.dataAbertura = dataA;
+        this.data.dia = d;
+        this.data.mes = m;
+        this.data.ano = a;
         this.agencia = agenciA;
         this.numero = numerO;
         this.saldo = saldO;
@@ -20,8 +41,25 @@ public class ContaExercicio {
 
     //Exibir informações da conta
     public void recuperaDadosParaImpressao() {
-        System.out.println("INFORMAÇÕES DA CONTA\n=-=-=-=-=-=-=-=-=-=-=-=\nNome do titular: " + nomeTitular + "\nAgência: " + agencia + "\nNúmero da conta: " + numero + "\nSaldo: R$" + saldo + "\nData de Abertura: " + dataAbertura);
-    }
+
+    System.out.println("INFORMAÇÕES DA CONTA\n=-=-=-=-=-=-=-=-=-=-=-");
+
+    System.out.printf(
+        "Nome do Titular: %s%n" +
+        "Agência: %s%n" +
+        "Número da Conta: %d%n" +
+        "Saldo: R$%.2f%n" +
+        "Data de Abertura: %02d/%02d/%04d%n",
+        
+        this.nomeTitular,
+        this.agencia,
+        this.numero,
+        this.saldo,
+        this.data.dia,
+        this.data.mes,
+        this.data.ano
+    );
+}
 
     //Métodos de funções da minha classe:
     public void sacar (double valor) {
--- StartPrograma.java
+++ StartPrograma.java
@@ -1,9 +1,9 @@
 class StartPrograma {
     public static void main(String[] args) {
-        ContaExercicio c1 = new ContaExercicio("Anderson B.", "22/09/2026", "A01", 123, 50000.00);
-        ContaExercicio c2 = new ContaExercicio("Davilla S.", "23/09/2026", "A02", 456, 60000);
-        ContaExercicio c3 = c2;
-        c1.compararContas (c2, c3);
+        ContaExercicio c1 = new ContaExercicio("Anderson B.", 27, 1, 2002 , "A01", 123, 50000.00);
+        ContaExercicio c2 = new ContaExercicio("Davilla S.", 3, 9, 2010 , "A02", 456, 60000);
+        
+        c1.recuperaDadosParaImpressao();
 
 
     }
